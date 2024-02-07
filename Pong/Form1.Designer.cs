128edf878ba27bc250c61400acadbc5d3aa2116d
Koen Gerber <koengerb477@outlook.com>
2024-02-07 09:42:00 -0500

    Good Stuff

--- Pong/Form1.Designer.cs
+++ Pong/Form1.Designer.cs
@@ -33,6 +33,10 @@
             this.startLabel = new System.Windows.Forms.Label();
             this.player1ScoreLabel = new System.Windows.Forms.Label();
             this.plaery2ScoreLabel = new System.Windows.Forms.Label();
+            this.powerUpA1Label = new System.Windows.Forms.Label();
+            this.powerUpA2Label = new System.Windows.Forms.Label();
+            this.powerUpB1Label = new System.Windows.Forms.Label();
+            this.powerUpB2Label = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // gameUpdateLoop
@@ -47,9 +51,10 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.startLabel.Font = new System.Drawing.Font("Courier New", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.startLabel.ForeColor = System.Drawing.Color.White;
-            this.startLabel.Location = new System.Drawing.Point(141, 130);
+            this.startLabel.Location = new System.Drawing.Point(212, 200);
+            this.startLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.startLabel.Name = "startLabel";
-            this.startLabel.Size = new System.Drawing.Size(329, 160);
+            this.startLabel.Size = new System.Drawing.Size(494, 246);
             this.startLabel.TabIndex = 0;
             this.startLabel.Text = "Press Space To Start or Esc to Exit";
             this.startLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -59,11 +64,13 @@
             this.player1ScoreLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
+            this.player1ScoreLabel.BackColor = System.Drawing.Color.Transparent;
             this.player1ScoreLabel.Font = new System.Drawing.Font("Courier New", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.player1ScoreLabel.ForeColor = System.Drawing.Color.White;
-            this.player1ScoreLabel.Location = new System.Drawing.Point(222, 9);
+            this.player1ScoreLabel.Location = new System.Drawing.Point(333, 14);
+            this.player1ScoreLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.player1ScoreLabel.Name = "player1ScoreLabel";
-            this.player1ScoreLabel.Size = new System.Drawing.Size(64, 68);
+            this.player1ScoreLabel.Size = new System.Drawing.Size(96, 105);
             this.player1ScoreLabel.TabIndex = 1;
             this.player1ScoreLabel.Text = "0";
             this.player1ScoreLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -73,26 +80,97 @@
             this.plaery2ScoreLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
+            this.plaery2ScoreLabel.BackColor = System.Drawing.Color.Transparent;
             this.plaery2ScoreLabel.Font = new System.Drawing.Font("Courier New", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.plaery2ScoreLabel.ForeColor = System.Drawing.Color.White;
-            this.plaery2ScoreLabel.Location = new System.Drawing.Point(322, 9);
+            this.plaery2ScoreLabel.Location = new System.Drawing.Point(483, 14);
+            this.plaery2ScoreLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.plaery2ScoreLabel.Name = "plaery2ScoreLabel";
-            this.plaery2ScoreLabel.Size = new System.Drawing.Size(64, 68);
+            this.plaery2ScoreLabel.Size = new System.Drawing.Size(96, 105);
             this.plaery2ScoreLabel.TabIndex = 2;
             this.plaery2ScoreLabel.Text = "0";
             this.plaery2ScoreLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
+            // powerUpA1Label
+            // 
+            this.powerUpA1Label.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.powerUpA1Label.BackColor = System.Drawing.Color.Red;
+            this.powerUpA1Label.Font = new System.Drawing.Font("Courier New", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.powerUpA1Label.ForeColor = System.Drawing.Color.White;
+            this.powerUpA1Label.Location = new System.Drawing.Point(13, 14);
+            this.powerUpA1Label.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.powerUpA1Label.Name = "powerUpA1Label";
+            this.powerUpA1Label.Size = new System.Drawing.Size(53, 57);
+            this.powerUpA1Label.TabIndex = 3;
+            this.powerUpA1Label.Text = "A";
+            this.powerUpA1Label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // powerUpA2Label
+            // 
+            this.powerUpA2Label.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.powerUpA2Label.BackColor = System.Drawing.Color.Red;
+            this.powerUpA2Label.Font = new System.Drawing.Font("Courier New", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.powerUpA2Label.ForeColor = System.Drawing.Color.White;
+            this.powerUpA2Label.Location = new System.Drawing.Point(771, 14);
+            this.powerUpA2Label.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.powerUpA2Label.Name = "powerUpA2Label";
+            this.powerUpA2Label.Size = new System.Drawing.Size(53, 57);
+            this.powerUpA2Label.TabIndex = 4;
+            this.powerUpA2Label.Text = "<";
+            this.powerUpA2Label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // powerUpB1Label
+            // 
+            this.powerUpB1Label.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.powerUpB1Label.BackColor = System.Drawing.Color.Red;
+            this.powerUpB1Label.Font = new System.Drawing.Font("Courier New", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.powerUpB1Label.ForeColor = System.Drawing.Color.White;
+            this.powerUpB1Label.Location = new System.Drawing.Point(84, 14);
+            this.powerUpB1Label.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.powerUpB1Label.Name = "powerUpB1Label";
+            this.powerUpB1Label.Size = new System.Drawing.Size(53, 57);
+            this.powerUpB1Label.TabIndex = 5;
+            this.powerUpB1Label.Text = "D";
+            this.powerUpB1Label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // powerUpB2Label
+            // 
+            this.powerUpB2Label.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.powerUpB2Label.BackColor = System.Drawing.Color.Red;
+            this.powerUpB2Label.Font = new System.Drawing.Font("Courier New", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.powerUpB2Label.ForeColor = System.Drawing.Color.White;
+            this.powerUpB2Label.Location = new System.Drawing.Point(844, 14);
+            this.powerUpB2Label.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.powerUpB2Label.Name = "powerUpB2Label";
+            this.powerUpB2Label.Size = new System.Drawing.Size(53, 57);
+            this.powerUpB2Label.TabIndex = 6;
+            this.powerUpB2Label.Text = ">";
+            this.powerUpB2Label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
-            this.ClientSize = new System.Drawing.Size(616, 450);
+            this.ClientSize = new System.Drawing.Size(924, 692);
+            this.Controls.Add(this.powerUpB2Label);
+            this.Controls.Add(this.powerUpB1Label);
+            this.Controls.Add(this.powerUpA2Label);
+            this.Controls.Add(this.powerUpA1Label);
             this.Controls.Add(this.plaery2ScoreLabel);
             this.Controls.Add(this.player1ScoreLabel);
             this.Controls.Add(this.startLabel);
             this.DoubleBuffered = true;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Pong";
@@ -109,6 +187,11 @@
         private System.Windows.Forms.Label startLabel;
         private System.Windows.Forms.Label player1ScoreLabel;
         private System.Windows.Forms.Label plaery2ScoreLabel;
+        private System.Windows.Forms.Timer powerUpTimer;
+        private System.Windows.Forms.Label powerUpA1Label;
+        private System.Windows.Forms.Label powerUpA2Label;
+        private System.Windows.Forms.Label powerUpB1Label;
+        private System.Windows.Forms.Label powerUpB2Label;
     }
 }
 
--- Pong/Form1.cs
+++ Pong/Form1.cs
@@ -16,6 +16,7 @@ using System.Text;
 using System.Windows.Forms;
 using System.Threading;
 using System.Media;
+using System.Diagnostics;
 
 #endregion
 
@@ -34,7 +35,7 @@ namespace Pong
         SoundPlayer collisionSound = new SoundPlayer(Properties.Resources.collision);
 
         //determines whether a key is being pressed or not
-        Boolean wKeyDown, sKeyDown, upKeyDown, downKeyDown;
+        Boolean wKeyDown, sKeyDown, upKeyDown, downKeyDown, aKeyDown, dKeyDown, leftKeyDown, rightKeyDown;
 
         // check to see if a new game can be started
         Boolean newGameOk = true;
@@ -42,23 +43,33 @@ namespace Pong
         //ball values
         Boolean ballMoveRight = true;
         Boolean ballMoveDown = true;
-        const int BALL_SPEED = 4;
+        const int BALL_SPEED = 5;
         const int BALL_WIDTH = 20;
-        const int BALL_HEIGHT = 20; 
+        const int BALL_HEIGHT = 20;
         Rectangle ball;
 
         //player values
-        const int PADDLE_SPEED = 4;
+        const int PADDLE_SPEED = 5;
         const int PADDLE_EDGE = 20;  // buffer distance between screen edge and paddle            
         const int PADDLE_WIDTH = 10;
         const int PADDLE_HEIGHT = 40;
         Rectangle player1, player2;
+
+        //blocker values
+        const int BLOCKER_SPEED = 6;
+        const int BLOCKER_WIDTH = 10;
+        const int BLOCKER_HEIGHT = 100;
+        Rectangle blocker;
+        Boolean blockerMoveDown = true;
 
         //player and game scores
         int player1Score = 0;
         int player2Score = 0;
         int gameWinScore = 2;  // number of points needed to win game
 
+        Stopwatch powerUpTiming = new Stopwatch();
+        Boolean powerUpA = false;
+        Boolean powerUpB = false;
         #endregion
 
         public Form1()
@@ -77,11 +88,23 @@ namespace Pong
                 case Keys.S:
                     sKeyDown = true;
                     break;
+                case Keys.A:
+                    aKeyDown = true;
+                    break;
+                case Keys.D:
+                    dKeyDown = true;
+                    break;
                 case Keys.Up:
                     upKeyDown = true;
                     break;
                 case Keys.Down:
                     downKeyDown = true;
+                    break;
+                case Keys.Left:
+                    leftKeyDown = true;
+                    break;
+                case Keys.Right:
+                    rightKeyDown = true;
                     break;
                 case Keys.Y:
                 case Keys.Space:
@@ -98,7 +121,7 @@ namespace Pong
                     break;
             }
         }
-        
+
         private void Form1_KeyUp(object sender, KeyEventArgs e)
         {
             //check to see if a key has been released and set its KeyDown value to false if it has
@@ -116,6 +139,18 @@ namespace Pong
                 case Keys.Down:
                     downKeyDown = false;
                     break;
+                case Keys.A:
+                    aKeyDown = false;
+                    break;
+                case Keys.D:
+                    dKeyDown = false;
+                    break;
+                case Keys.Left:
+                    leftKeyDown = false;
+                    break;
+                case Keys.Right:
+                    rightKeyDown = false;
+                    break;
             }
         }
 
@@ -130,6 +165,8 @@ namespace Pong
                 newGameOk = false;
                 startLabel.Visible = false;
                 gameUpdateLoop.Start();
+                player1ScoreLabel.Text = Convert.ToString(player1Score);
+                plaery2ScoreLabel.Text = Convert.ToString(player2Score);
             }
 
             //player start positions
@@ -137,7 +174,10 @@ namespace Pong
             player2 = new Rectangle(this.Width - PADDLE_EDGE - PADDLE_WIDTH, this.Height / 2 - PADDLE_HEIGHT / 2, PADDLE_WIDTH, PADDLE_HEIGHT);
 
             // TODO create a ball rectangle in the middle of screen
+            ball = new Rectangle(this.Width / 2 - BALL_WIDTH / 2, this.Height / 2 - BALL_HEIGHT / 2, BALL_WIDTH, BALL_HEIGHT);
 
+            //blocker start positions
+            blocker = new Rectangle(this.Width / 2 - BLOCKER_WIDTH / 2, 0, BLOCKER_WIDTH, BLOCKER_HEIGHT);
         }
 
         /// <summary>
@@ -149,9 +189,23 @@ namespace Pong
             #region update ball position
 
             // TODO create code to move ball either left or right based on ballMoveRight and using BALL_SPEED
-
+            if (ballMoveRight)
+            {
+                ball.X += BALL_SPEED;
+            }
+            else
+            {
+                ball.X -= BALL_SPEED;
+            }
             // TODO create code move ball either down or up based on ballMoveDown and using BALL_SPEED
-
+            if (ballMoveDown)
+            {
+                ball.Y += BALL_SPEED;
+            }
+            else
+            {
+                ball.Y -= BALL_SPEED;
+            }
             #endregion
 
             #region update paddle positions
@@ -159,14 +213,23 @@ namespace Pong
             if (wKeyDown == true && player1.Y > 0)
             {
                 // TODO create code to move player 1 up
+                player1.Y -= PADDLE_SPEED;
             }
-
             // TODO create an if statement and code to move player 1 down 
-
+            if (sKeyDown == true && player1.Y < this.Height - PADDLE_HEIGHT)
+            {
+                player1.Y += PADDLE_SPEED;
+            }
             // TODO create an if statement and code to move player 2 up
-
+            if (upKeyDown == true && player2.Y > 0)
+            {
+                player2.Y -= PADDLE_SPEED;
+            }
             // TODO create an if statement and code to move player 2 down
-
+            if (downKeyDown == true && player2.Y < this.Height - PADDLE_HEIGHT)
+            {
+                player2.Y += PADDLE_SPEED;
+            }
             #endregion
 
             #region ball collision with top and bottom lines
@@ -174,27 +237,57 @@ namespace Pong
             if (ball.Y < 0) // if ball hits top line
             {
                 // TODO use ballMoveDown boolean to change direction
+                ballMoveDown = true;
                 // TODO play a collision sound
+                collisionSound.Play();
             }
             // TODO In an else if statement check for collision with bottom line
             // If true use ballMoveDown boolean to change direction
+            else if (ball.Y > this.Height - BALL_HEIGHT)
+            {
+                ballMoveDown = false;
+
+                collisionSound.Play();
+            }
 
             #endregion
 
             #region ball collision with paddles
 
             // TODO create if statment that checks if player1 collides with ball and if it does
-                 // --- play a "paddle hit" sound and
-                 // --- use ballMoveRight boolean to change direction
-
+            // --- play a "paddle hit" sound and
+            // --- use ballMoveRight boolean to change direction
+            //if (player1.IntersectsWith(ball))
+            //{
+            //    ballMoveRight = true;
+            //    collisionSound.Play();
+            //}
             // TODO create if statment that checks if player2 collides with ball and if it does
-                // --- play a "paddle hit" sound and
-                // --- use ballMoveRight boolean to change direction
-            
+            // --- play a "paddle hit" sound and
+            // --- use ballMoveRight boolean to change direction
+            //if (player2.IntersectsWith(ball))
+            //{
+            //    ballMoveRight = false;
+            //    collisionSound.Play(); 
+            //}
             /*  ENRICHMENT
              *  Instead of using two if statments as noted above see if you can create one
              *  if statement with multiple conditions to play a sound and change direction
              */
+            if (player1.IntersectsWith(ball) || player2.IntersectsWith(ball))
+            {
+                collisionSound.Play();
+                ballMoveRight = !ballMoveRight;
+                if (ballMoveRight)
+                {
+                    ball.X = player1.X + PADDLE_WIDTH;
+                }
+                else
+                {
+                    ball.X = player2.X - BALL_WIDTH;
+                }
+
+            }
 
             #endregion
 
@@ -205,20 +298,133 @@ namespace Pong
                 // TODO
                 // --- play score sound
                 // --- update player 2 score and display it to the label
-
+                scoreSound.Play();
+                player2Score++;
+                plaery2ScoreLabel.Text = Convert.ToString(player2Score);
                 // TODO use if statement to check to see if player 2 has won the game. If true run 
                 // GameOver() method. Else change direction of ball and call SetParameters() method.
-
+                if (player2Score > gameWinScore)
+                {
+                    GameOver("Player 2");
+                }
+                else
+                {
+                    ballMoveRight = true;
+                    SetParameters();
+                }
             }
 
             // TODO same as above but this time check for collision with the right wall
-
+            if (ball.X > this.Width - BALL_WIDTH)  // ball hits right wall logic
+            {
+                // TODO
+                // --- play score sound
+                // --- update player 1 score and display it to the label
+                scoreSound.Play();
+                player1Score++;
+                player1ScoreLabel.Text = Convert.ToString(player1Score);
+                // TODO use if statement to check to see if player 1 has won the game. If true run 
+                // GameOver() method. Else change direction of ball and call SetParameters() method.
+                if (player1Score > gameWinScore)
+                {
+                    GameOver("Player 1");
+                }
+                else
+                {
+                    ballMoveRight = false;
+                    SetParameters();
+                }
+            }
             #endregion
-            
+
+            #region blocker movement
+            if (blockerMoveDown)
+            {
+                blocker.Y += BLOCKER_SPEED;
+            }
+            else
+            {
+                blocker.Y -= BLOCKER_SPEED;
+            }
+            #endregion
+
+            #region blocker change direction
+            if (blocker.Y > this.Height - BLOCKER_HEIGHT || blocker.Y < 0)
+            {
+                blockerMoveDown = !blockerMoveDown;     
+            }
+            #endregion
+
+            #region ball collides with blocker
+            if (ball.IntersectsWith(blocker))
+            {
+                ballMoveRight = !ballMoveRight;
+            }
+            #endregion
+
+            #region cooldown on power ups
+            if (powerUpTiming.ElapsedMilliseconds > 5000)
+            {
+                powerUpA1Label.Visible = true;
+                powerUpA2Label.Visible = true;
+                powerUpB1Label.Visible = true;
+                powerUpB2Label.Visible = true;
+            }
+            #endregion
+
+            #region using powerups
+            if ((aKeyDown || leftKeyDown) && powerUpTiming.ElapsedMilliseconds > 5000 || powerUpTiming.ElapsedMilliseconds == 0)
+            {
+                powerUpTiming.Reset();
+                powerUpTiming.Start();
+                powerUpA = true;
+
+                if (aKeyDown)
+                {
+                    powerUpA1Label.Visible = false;
+                }
+                else if (leftKeyDown)
+                {
+                    powerUpA2Label.Visible = false;
+                }
+            }
+            if ((dKeyDown || rightKeyDown) && powerUpTiming.ElapsedMilliseconds > 5000 || powerUpTiming.ElapsedMilliseconds == 0)
+            {
+                powerUpTiming.Reset();
+                powerUpTiming.Start();
+                powerUpB = true;
+
+                if (dKeyDown)
+                {
+                    powerUpB1Label.Visible = false;
+                }
+                else if (rightKeyDown)
+                {
+                    powerUpB2Label.Visible = false;
+                }
+            }
+
+            if (powerUpA && powerUpTiming.ElapsedMilliseconds < 2000)
+            {
+                blocker.Height = this.Height;
+                blocker.Y = 0;
+            }
+            else if (powerUpB && powerUpTiming.ElapsedMilliseconds < 2000)
+            {
+                blocker.Height = 0;
+            }
+
+            if(powerUpTiming.ElapsedMilliseconds > 2000)
+            {
+                blocker.Height = BLOCKER_HEIGHT;
+                powerUpA = false;
+                powerUpB = false;
+            }
+            #endregion
             //refresh the screen, which causes the Form1_Paint method to run
             this.Refresh();
         }
-        
+
         /// <summary>
         /// Displays a message for the winner when the game is over and allows the user to either select
         /// to play again or end the program
@@ -230,18 +436,25 @@ namespace Pong
 
             // TODO create game over logic
             // --- stop the gameUpdateLoop
+            gameUpdateLoop.Stop();
             // --- show a message on the startLabel to indicate a winner, (may need to Refresh).
+            startLabel.Visible = true;
+            startLabel.Text = $"{winner} Wins!!!\n";
             // --- use the startLabel to ask the user if they want to play again
-
+            startLabel.Text += "Press Space to Start or Esc to Exit";
         }
 
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
             // TODO draw player2 using FillRectangle
             e.Graphics.FillRectangle(whiteBrush, player1);
+            e.Graphics.FillRectangle(whiteBrush, player2);
 
             // TODO draw ball using FillRectangle
+            e.Graphics.FillRectangle(whiteBrush, ball);
 
+            //draw blocker
+            e.Graphics.FillRectangle(whiteBrush, blocker);
         }
 
     }
